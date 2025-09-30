0f1c9c0902ae517a99c3d75c466fee8519d09357
eohler2003 <ellenohler@uchicago.edu>
2025-09-30 11:23:05 -0500

    Create script.py

---
+++ script.py
@@ -0,0 +1,16 @@
+# PA00
+
+## About Me
+Hello, my name is **Ellen** and I am a MAPSS political science student. I completed my undergraduate degrees in political science and economics at UCLA and hope to pursue a PhD in political science. My research focuses on the relationship between social media and political attitudes. Here are a few of the things I enjoy doing in my free time:
+* travelling
+* visiting art museums
+* playing music 
+* spending time with friends and family
+
+## Picture
+!([paris-picture](https://github.com/macs30111/pa0-eohler2003/blob/main/paris_picture.JPG))
+_Me in Paris this summer at the Musee de l'Orangerie._
+
+
+## Summary and Reflection of the Git/GitHub workflow
+To edit this README file, I clicked on the pencil icon in the top right-hand corner. I then added headers, copying the syntax of the existing headers in the file. I then wrote the blurbs about myself and about my workflow, and bolded and italicized certain words and phrases using keyboard shortcuts. This taught me that bolded words can be made by placing two asterisks before and after the word and than italicized words can be made by adding underscores. Through watching the [intro video on Github](https://www.youtube.com/watch?v=w3jLJU7DT5E&feature=youtu.be), I learned that Github allows users to keep track of their own code, and selectively implement the code of others, in a systematic and organized way. This is a valuable tool and I'm excited to explore further. 
